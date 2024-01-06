69d8261f351dd2b91734a6e85103397b53553481
Efrenams9 <efrenale5@gmail.com>
2024-01-05 18:36:30 -0600

    1ra 05-enero-24

--- cr-pi-countries-main/server/src/Controllers/getActivities.js
+++ cr-pi-countries-main/server/src/Controllers/getActivities.js
@@ -9,11 +9,42 @@
 // module.exports = getActivities;
 
                         //resolver
-const getActivities = async ()=>{
-                                //rechazo
+// const getActivities = async ()=>{
+//                                 //rechazo
 
-    try {
-    const activities = await Activity.findAll();
+//     try {
+//     const activities = await Activity.findAll();
+//     res.json(activities);
+//   } catch (error) {
+//     console.error('Error getting activities:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// };
+
+// // console.log(getActivities());
+// module.exports = getActivities;
+
+////////////////////////////////////////////////////////////////////////////////////
+
+const {Country, Activity} = require('../db');
+const getActivities = async (req, res) => {
+  const countryId = req.params.idPais;
+
+  try {
+    const country = await Country.findOne({
+      where: {
+        id: countryId,
+      },
+      include: {model: Activity},
+    });
+
+    if (!country) {
+      res.status(404).json({ error: 'Country not found' });
+      return;
+    }
+
+    const activities = country.Activities;
+
     res.json(activities);
   } catch (error) {
     console.error('Error getting activities:', error);
@@ -21,5 +52,4 @@ const getActivities = async ()=>{
   }
 };
 
-// console.log(getActivities());
 module.exports = getActivities;
